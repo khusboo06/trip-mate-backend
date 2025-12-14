e1e1535ba419cd7266bb21ca6a644e021af7fe38
Khushboo Kumari <khushbookumari200314@gmail.com>
2025-12-15 02:31:59 +0530

    minor changes

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,4 @@
-// // backend/utils/email.js
-// const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   host: process.env.EMAIL_HOST,      // e.g. smtp.gmail.com
-//   port: process.env.EMAIL_PORT,      // e.g. 587
-//   secure: false,                     // false for 587
-//   auth: {
-//     user: process.env.EMAIL_USER,    // your Gmail
-//     pass: process.env.EMAIL_PASS,    // your app password
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// module.exports = transporter;
-
-
-
-// backend/utils/email.js
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
